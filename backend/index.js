9d8603d387f4b046154c7c142934fbd150895e63
Tanuja Shekokar <tanu.shekokar@gmail.com>
2025-07-18 22:59:56 +0530

    change index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors({
   origin: ["https://leaderboard-app-frontend-cyt8.onrender.com"],
